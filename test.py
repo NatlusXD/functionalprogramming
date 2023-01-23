dd28dc8112b15a304cbdc67a216fe7333f4127a2
Georgii Statenin <gosh.03@outlook.com>
2023-01-23 09:22:05 +0600

    python program

---
+++ test.py
@@ -0,0 +1,12 @@
+"""
+Python program
+If/else
+"""
+#input your text
+text = input('Введите ваш текст: ')
+if text == 'имя':
+    print('Георгий')
+elif text == 'фамилия':
+    print('Статенин')
+else:
+    print('Статенин Георгий, студент SU')
