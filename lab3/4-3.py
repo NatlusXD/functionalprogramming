dd68ea2fcd405cf5914e500353add2e3e04e9cc8
Georgii Statenin <gosh.03@outlook.com>
2023-01-30 09:25:37 +0600

    lab3

--- lab3/4-3.py
+++ lab3/4-3.py
@@ -1,8 +1,8 @@
 A = int(input("Enter A: "))
 B = int(input("Enter B: "))
 
-while A%2 != 1:
+while A % 2 != 1:
     A -= 1
 
-for num in range(A, B-1, -2):
+for num in range(A, B - 1, -2):
     print(num)
