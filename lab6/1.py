eae1007965c2c2cdebca7a90be0bb35d9b55664a
Georgii Statenin <gosh.03@outlook.com>
2023-02-20 08:49:11 +0600

    lab6

--- lab6/1.py
+++ lab6/1.py
@@ -4,16 +4,10 @@ def create_tuple(length, start, end):
 
     return tuple(random.randint(start, end) for _ in range(length))
 
-# Создаем два кортежа с помощью функции create_tuple
 tuple1 = create_tuple(10, 0, 5)
 tuple2 = create_tuple(10, -5, 0)
 
-# Объединяем два кортежа в третий кортеж с помощью оператора +
 tuple3 = tuple1 + tuple2
 
-# Используем метод count() для подсчета количества нулей в третьем кортеже
-count_zeroes = tuple3.count(0)
-
-# Выводим третий кортеж и количество нулей в нем
 print("Третий кортеж: ", tuple3)
-print("Количество нулей в третьем кортеже: ", count_zeroes)
+print("Количество нулей в третьем кортеже: ", tuple3.count(0))
